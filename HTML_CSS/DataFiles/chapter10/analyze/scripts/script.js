812a83513fc78c0ab9ff9d7bbcd8ee974ff9576d
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-13 11:52:06 -0500

    analyze

--- HTML_CSS/DataFiles/chapter10/analyze/scripts/script.js
+++ HTML_CSS/DataFiles/chapter10/analyze/scripts/script.js
@@ -1,12 +1,13 @@
 /*
-    Student Name:
+    Student Name: Justin Thompson
     File Name: script.js
-    Date: 
+    Date: 10/13/2025
 */
 
 //Function to display the first picture
-function pic1 {
+function pic1() {
     imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block"
     figCap.textContent = "Trunk Bay in St. John";
+}
