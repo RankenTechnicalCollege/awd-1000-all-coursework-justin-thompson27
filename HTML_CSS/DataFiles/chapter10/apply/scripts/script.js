3ceac01a91baa5c9271945ae188d0412d380facf
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-13 11:45:24 -0500

    done

---
+++ HTML_CSS/DataFiles/chapter10/apply/scripts/script.js
@@ -0,0 +1,11 @@
+/* Student Name: Justin Thompson
+Date: 10/13/25
+File Name: script.js */
+
+// Function to Display Content 
+function content(){
+  var text = document.getElementById("new")
+  text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function.";
+  text.style.color = "#c0145f";
+  text.style.fontSize = "2em";
+}
