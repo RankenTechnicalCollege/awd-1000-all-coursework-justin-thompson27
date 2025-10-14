62abb8fd61d0cee9d2cf7f3637b6d512b1b2d188
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-14 10:08:30 -0500

    done

---
+++ HTML_CSS/Labs/HTML_CH02_Labs/Lab1/music/scripts/script.js
@@ -0,0 +1,18 @@
+/* 
+Student Name: Justin Thompson
+File Name: script.js
+Date: 10/14/2025 */
+
+
+//Hamburger menu function
+
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  if(menu.style.display ==="block"){
+    menu.style.display = "none";
+  }
+  else{
+    menu.style.display = "block"
+  }
+
+}
