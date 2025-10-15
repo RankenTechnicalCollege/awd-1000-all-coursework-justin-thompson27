2e50a3c52eadad2e9fff9930bf231fdb0b8f59b0
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-15 11:41:03 -0500

    extend

--- HTML_CSS/DataFiles/chapter10/extend/scripts/script.js
+++ HTML_CSS/DataFiles/chapter10/extend/scripts/script.js
@@ -1,5 +1,15 @@
 /*
-    Student Name:
+    Student Name: Justin Thompson
     File Name: script.js
-    Date: 
+    Date: 10/15/2025
 */
+
+$(document).ready(function(){
+  $("#show").click(function(){
+    $("p").show(1000);
+  });
+  $("#hide").click(function(){
+    $("p").hide(1000);
+  });
+
+});
