f4591dd409f426449f99641655d084a28cc85591
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-23 11:54:02 -0500

    2-2

--- JavaScript/data-files/Chapter02/js02/project02/project02-02_txt.js
+++ JavaScript/data-files/Chapter02/js02/project02/project02-02_txt.js
@@ -3,9 +3,18 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Justin Thompson
+      Date:   10/23/2025
 
       Filename: project02-02.js
  */
  
+      function verifyForm(){
+            let name = document.getElementById('name').value;
+            let email = document.getElementById('email').value;
+            let phone = document.getElementById('phone').value;
+
+            name && email && phone ? window.alert("Thank you") : window.alert("Please fill in all fields.") ;
+      }
+
+      document.getElementById('submit').addEventListener("click", verifyForm);
