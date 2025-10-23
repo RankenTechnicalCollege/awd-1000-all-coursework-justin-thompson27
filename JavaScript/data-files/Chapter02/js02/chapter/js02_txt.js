876ff9b818bc9e74e9297fd4b977113ac094bfe8
Justin Thompson <Justin_Thompson@insideranken.org>
2025-10-23 10:06:56 -0500

    homework

--- JavaScript/data-files/Chapter02/js02/chapter/js02_txt.js
+++ JavaScript/data-files/Chapter02/js02/chapter/js02_txt.js
@@ -10,3 +10,53 @@
       Filename: js02.js
  */
 
+'use strict'
+//Global variables
+const EMP_COST = 100;
+const BOOK_COST = 350;
+const REPRO_COST = 1250;
+const TRAVEL_COST = 2;
+
+      window.addEventListener("load", setupForm);
+
+function setupForm(){
+
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+
+      //Event Handlers
+      document.getElementById("photoNum").onchange = getEstimate;
+      document.getElementById("photoHrs").onchange = getEstimate;
+      document.getElementById("photoDist").onchange = getEstimate;
+      document.getElementById("makeBook").onchange = getEstimate;
+      document.getElementById("photoRights").onchange = getEstimate;
+
+      getEstimate();
+
+}
+
+function getEstimate(){
+      let totalCost = 0;
+      let photographers = document.getElementById("photoNum").value;
+      let hours = document.getElementById("photoHrs").value;
+      let distance = document.getElementById("photoDist").value;
+      let buyBook = document.getElementById("makeBook").checked;
+      let BuyRights = document.getElementById("photoRights").checked;
+
+
+
+      totalCost += photographers * hours * EMP_COST;
+      
+      totalCost += photographers * distance * TRAVEL_COST;
+
+      totalCost += buyBook ? BOOK_COST : 0;
+
+      totalCost += BuyRights ? REPRO_COST : 0;
+
+      document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
+
+
--- JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
+++ JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
@@ -3,9 +3,19 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Justin Thompson
+      Date:   10/23/2025
 
       Filename: project02-01.js
  */
 
+
+      FahrenheitToCelsius(degree)
+      {
+            return degree - 32 % 1.8;
+      }
+
+      CelsiusToFahrenheit(degree)
+      {
+            return degree * 1.8 + 32;
+      }
