74e09dd47af9d8860def1ff46dccd28f60de4bba
Justin Thompson <Justin_Thompson@insideranken.org>
2025-11-13 12:07:18 -0600

    5 chapter 3

--- JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
+++ JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
@@ -4,9 +4,26 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: 
-      Date:   
+      Author: Justin Thompson
+      Date:   11/13/25
 
       Filename: project05-02.js
 */
+let images = document.getElementsByTagName("img");
+let photoBucket = document.getElementById("photo_bucket");
+let photoList = document.getElementById("photo_list");
 
+for (let i = 0; i < images.length; i++) {
+    images[i].onclick = function(){
+      if (this.parentNode.id === "photo_bucket") {
+            let newItem = document.createElement("li");
+            photoList.appendChild(newItem);
+            newItem.appendChild(this);
+      } else {
+            let oldItem = this.parentNode;
+            photoBucket.appendChild(this);
+            oldItem.parentNode.removeChild(oldItem);
+      }
+    }
+      
+}
--- JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
+++ JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
@@ -4,9 +4,31 @@
       Project 05-03
 
       Project to create a table of headings from an article
-      Author: 
-      Date:   
+      Author: Justin Thompson
+      Date:   11/13/2025
+
 
       Filename: project05-03.js
 */
+let sourceDoc = document.getElementById("source_doc");
+let toc = document.getElementById("toc");
+let headingCount = 1;
+const heading = "H2";
+
+
+ for(let n = sourceDoc.firstElementChild; n != null;n= n.nextElementSibling){
+      if (n.nodeName === heading) {
+            let anchor = document.createElement("a");
+            anchor.name="doclink" + headingCount;
+            n.insertBefore(anchor , n.firstElementChild);
+            let listItem = document.createElement("li");
+            let link = document.createElement("a");
+            listItem.appendChild(link);
+            link.textContent = n.textContent;
+            listItem.href = "#doclink" + headingCount;
+            toc.appendChild(listItem);
+            headingCount++;
+      } 
+
+} 
 
