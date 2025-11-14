a52d637b049f999c4355405f8b8c59206dd1cab7
JKace93041 <jt3667316@gmail.com>
2025-11-13 22:34:13 -0600

    homework

--- JavaScript/data-files/Chapter05/js05/project04/project05-04_txt.js
+++ JavaScript/data-files/Chapter05/js05/project04/project05-04_txt.js
@@ -4,8 +4,8 @@
       Project 05-04
 
       Project to display footnotes in a popup window
-      Author: 
-      Date:   
+      Author: Justin Thompson
+      Date:   11/13/25
 
       Filename: project05-04.js
 */
@@ -13,3 +13,27 @@
 // Node list of phrases that are associated with footnotes
 let phrases = document.querySelectorAll("article blockquote dfn");
 
+
+for (let i = 0; i < phrases.length; i++) {
+     phrases[i].onclick = function(){
+      let phrase = document.createElement("h1");
+      phrase.textContent = this.textContent;
+      let footnote = document.createElement("p");
+      footnote.textContent = footnotes[i].textContent;
+      footnote.style = "font-style: italic; font-size 1.2em;"
+      let closeButton = document.createElement("input");
+      closeButton.type = "button";
+      closeButton.value = "Close Footnote";
+      closeButton.style = "display block; margin:  10px auto";
+      let popup = window.open("" , "footnote" , "width = 300 , height = 200 , top = 100 , left = 100");
+      popup.document.body.style = "background-color: ivory; font-size: 16px padding: 10px;"
+      popup.document.body.appendChild(phrase);
+       popup.document.body.appendChild(footnote); 
+       popup.document.body.appendChild(closeButton);
+       closeButton.onclick = function(){
+            popup.close();
+       }
+
+     }
+      
+}
